fecdbb628861a10ab8f20a03c920b841661d14f4
Elsa Zougouri <elsazougouri224@gmail.com>
2021-07-14 11:37:31 +0000

    adding index.html qnd completing app.js

--- app.js
+++ app.js
@@ -5,12 +5,26 @@
 //Require modules we use
 const express = require( "express" );
 const https = require( "https" );
+const bodyParser = require( "body-parser" ); 
+
+//Used and Useful constants
 const API_PORT = 3000;  //server port
+const apiKey = `1410bd7229098d1b2146ecebbe7e6b6f`;
+const units = "metric";
+
+//app to use
 const app = express();
+app.use( bodyParser.urlencoded( { extended:true } ) );  //parse the POST request
 
 //GET requests
 app.get( "/", ( req, res ) => {
-    const testURL = "https://api.openweathermap.org/data/2.5/weather?q=Abidjan&appid=1410bd7229098d1b2146ecebbe7e6b6f&lang=en&units=metric";
+    res.sendFile( `${__dirname}/index.html` );
+} );
+
+//POST requests
+app.post( "/", (req, res) => {
+    const queryCity = req.body.cityName;    //parsing the cityName that the user types
+    const testURL = `https://api.openweathermap.org/data/2.5/weather?q=${queryCity}&appid=${apiKey}&lang=en&units=${units}`;
     //Making GET request to an external server natively using HTTPS module
     https.get( testURL, ( response ) => {
         response.on( "data", ( data ) => {  //GET the response on "data" event and log the data
@@ -20,16 +34,16 @@ app.get( "/", ( req, res ) => {
             //Print the weather temperature, description and the img linked to it
             const temperature = weatherData.main.temp;
             const description = weatherData.weather[0].description;
-            const nameCity = weatherData.name;
             const icon = weatherData.weather[0].icon;
             const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
-            res.send( `<h1>The temperature in ${ nameCity } is ${ temperature } °C </h1>` 
-                      `<p>The weather is currently "${ description }" and there's an image : </p> <br />`
-                      `<img src="${iconURL}"> `);
+            res.send( "<h1>The temperature in " + queryCity + " is " + temperature + " °C </h1>" +
+                      "<p>The weather is currently " + description + " and there's an image : </p> <br />" +
+                      "<img src=" + iconURL + ">" );
         } );    
     } );
-} );
+} )
+
 
 //Spin up the server 
 app.listen( API_PORT, () => {
